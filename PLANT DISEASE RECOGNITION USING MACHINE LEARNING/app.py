ac66c073b721376f5fb89fd903656cd53a76eab0
raghad1410 <150474254+raghad1410@users.noreply.github.com>
2025-05-02 19:36:26 +0300

    Add files via upload

--- PLANT DISEASE RECOGNITION USING MACHINE LEARNING/app.py
+++ PLANT DISEASE RECOGNITION USING MACHINE LEARNING/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template,request,redirect,send_from_directory,url_for
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for
 import numpy as np
 import json
 import uuid
 import tensorflow as tf
+import os
 
 app = Flask(__name__)
 model = tf.keras.models.load_model("models/plant_disease_recog_model_pwp.keras")
@@ -49,18 +50,16 @@ label = ['Apple___Apple_scab',
 with open("plant_disease.json",'r') as file:
     plant_disease = json.load(file)
 
-# print(plant_disease[4])
-
 @app.route('/uploadimages/<path:filename>')
 def uploaded_images(filename):
     return send_from_directory('./uploadimages', filename)
 
-@app.route('/',methods = ['GET'])
+@app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
 def extract_features(image):
-    image = tf.keras.utils.load_img(image,target_size=(160,160))
+    image = tf.keras.utils.load_img(image, target_size=(160,160))
     feature = tf.keras.utils.img_to_array(image)
     feature = np.array([feature])
     return feature
@@ -68,25 +67,21 @@ def extract_features(image):
 def model_predict(image):
     img = extract_features(image)
     prediction = model.predict(img)
-    # print(prediction)
     prediction_label = plant_disease[prediction.argmax()]
     return prediction_label
 
-@app.route('/upload/',methods = ['POST','GET'])
+@app.route('/upload/', methods=['POST', 'GET'])
 def uploadimage():
     if request.method == "POST":
         image = request.files['img']
         temp_name = f"uploadimages/temp_{uuid.uuid4().hex}"
-        image.save(f'{temp_name}_{image.filename}')
-        print(f'{temp_name}_{image.filename}')
-        prediction = model_predict(f'./{temp_name}_{image.filename}')
-        return render_template('home.html',result=True,imagepath = f'/{temp_name}_{image.filename}', prediction = prediction )
-    
+        image_path = f'{temp_name}_{image.filename}'
+        image.save(image_path)
+        prediction = model_predict(image_path)
+        return render_template('home.html', result=True, imagepath=f'/{image_path}', prediction=prediction)
     else:
         return redirect('/')
-        
-    
+
 if __name__ == "__main__":
- import os
- port = int(os.environ.get("PORT", 10000))
- app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
