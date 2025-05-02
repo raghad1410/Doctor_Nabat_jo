1d75e9cb8c04c8331e1461a15c5bce1b8f211856
raghad1410 <150474254+raghad1410@users.noreply.github.com>
2025-05-02 19:34:13 +0300

    Update app.py

--- PLANT DISEASE RECOGNITION USING MACHINE LEARNING/app.py
+++ PLANT DISEASE RECOGNITION USING MACHINE LEARNING/app.py
@@ -87,4 +87,6 @@ def uploadimage():
         
     
 if __name__ == "__main__":
-    app.run(debug=True)
+ import os
+ port = int(os.environ.get("PORT", 10000))
+ app.run(host='0.0.0.0', port=port, debug=False)
